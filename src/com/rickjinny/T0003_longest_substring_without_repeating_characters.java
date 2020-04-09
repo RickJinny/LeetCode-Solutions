4f7bf7955674ec1229291a88d5600db5c3ade772
RickJinny <rickjinny@126.com>
2020-04-09 23:45:10 +0800

    LeetCode T0004
    LeetCode T0004

---
+++ src/com/rickjinny/T0003_longest_substring_without_repeating_characters.java
@@ -0,0 +1,53 @@
+package com.rickjinny;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 3、无重复字符的最长子串
+ *
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串的长度。
+ *
+ * 示例 1:
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为3。
+ *
+ * 示例 2:
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为1。
+ *
+ * 示例 3:
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *
+ */
+public class T0003_longest_substring_without_repeating_characters {
+
+    /**
+     * 标签：滑动窗口
+     * 暴力解法时间复杂度较高，会达到 O(n^2), 故而采取滑动窗口的方法降低时间复杂度
+     * 定义一个 map 数据结构存储 (k, v)，其中 key 值为字符，value 值为字符位置 +1，加 1 表示从字符位置后一个才开始不重复
+     * 我们定义不重复子串的开始位置为 start，结束位置为 end
+     * 随着 end 不断遍历向后，会遇到与 [start, end] 区间内字符相同的情况，此时将字符作为 key 值，获取其 value 值，并更新 start，此时 [start, end] 区间内不存在重复字符
+     * 无论是否更新 start，都会更新其 map 数据结构和结果 ans。
+     * 时间复杂度：O(n)
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char temp = s.charAt(end);
+            if (map.containsKey(temp)) {
+                start = Math.max(map.get(temp), start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        return ans;
+    }
+}
