378c2ac532f7163fc7d65acbf333a7f87a6b0239
RickJinny <rickjinny@126.com>
2020-04-09 21:35:27 +0800

    LeetCode T0002
    LeetCode T0002

--- src/com/rickjinny/T0002_add_two_numbers.java
+++ src/com/rickjinny/T0002_add_two_numbers.java
@@ -6,7 +6,7 @@ package com.rickjinny;
  *
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
- * 
+ *
  * 示例：
  * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
  * 输出：7 -> 0 -> 8
@@ -15,7 +15,30 @@ package com.rickjinny;
 public class T0002_add_two_numbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1;
+        ListNode q = l2;
+        ListNode curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 
     private static class ListNode {
