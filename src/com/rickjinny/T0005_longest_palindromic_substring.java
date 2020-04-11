c89881323c0a6294f72e1fb446ad58f00a60bd47
RickJinny <rickjinny@126.com>
2020-04-11 11:37:42 +0800

    LeetCode T0005
    LeetCode T0005

---
+++ src/com/rickjinny/T0005_longest_palindromic_substring.java
@@ -0,0 +1,49 @@
+package com.rickjinny;
+
+/**
+ * 5、最长回文子串
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ *
+ * 示例 2：
+ * 输入: "cbbd"
+ * 输出: "bb"
+ */
+public class T0005_longest_palindromic_substring {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+        int strLen = s.length();
+        // 最长回文串的起点
+        int maxStart = 0;
+        // 最长回文串的终点
+        int maxEnd = 0;
+        // 最长回文串的长度
+        int maxLen = 1;  
+
+        boolean[][] dp = new boolean[strLen][strLen];
+
+        for (int r = 1; r < strLen; r++) {
+            for (int l = 0; l < r; l++) {
+                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
+                    dp[l][r] = true;
+                    if (r - l + 1 > maxLen) {
+                        maxLen = r - l + 1;
+                        maxStart = l;
+                        maxEnd = r;
+
+                    }
+                }
+
+            }
+
+        }
+        return s.substring(maxStart, maxEnd + 1);
+    }
+}
