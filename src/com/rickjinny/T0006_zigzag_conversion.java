f65bda28a6ede162d3fc6640cf924371eb943f64
RickJinny <rickjinny@126.com>
2020-04-13 23:03:50 +0800

    update
    update

--- src/com/rickjinny/T0006_zigzag_conversion.java
+++ src/com/rickjinny/T0006_zigzag_conversion.java
@@ -1,5 +1,8 @@
 package com.rickjinny;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 6、Z 字形变换
  * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
@@ -25,8 +28,27 @@ package com.rickjinny;
  * E   O E   I I
  * E C   I H   N
  * T     S     G
- *
  */
 public class T0006_zigzag_conversion {
 
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
 }
