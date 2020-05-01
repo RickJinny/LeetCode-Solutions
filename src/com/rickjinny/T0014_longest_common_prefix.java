4274a28fc75c91ea1fb6faf2177ee1dffd1de3f8
RickJinny <rickjinny@126.com>
2020-05-01 23:52:41 +0800

    LeetCode T0014
    LeetCode T0014

--- src/com/rickjinny/T0014_longest_common_prefix.java
+++ src/com/rickjinny/T0014_longest_common_prefix.java
@@ -19,6 +19,22 @@ package com.rickjinny;
 public class T0014_longest_common_prefix {
 
     public String longestCommonPrefix(String[] strs) {
-        return null;
+        if (strs.length == 0) {
+            return "";
+        }
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            for (; j < ans.length() && j < strs[i].length(); j++) {
+                if (ans.charAt(j) != strs[i].charAt(j)) {
+                    break;
+                }
+            }
+            ans = ans.substring(0, j);
+            if (ans.equals("")) {
+                return ans;
+            }
+        }
+        return ans;
     }
 }
