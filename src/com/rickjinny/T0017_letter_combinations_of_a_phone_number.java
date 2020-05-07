f2360813c3098a4f433630cb7769fc624bff94b5
RickJinny <rickjinny@126.com>
2020-05-07 23:50:04 +0800

    LeetCode T0017
    LeetCode T0017

--- src/com/rickjinny/T0017_letter_combinations_of_a_phone_number.java
+++ src/com/rickjinny/T0017_letter_combinations_of_a_phone_number.java
@@ -1,5 +1,6 @@
 package com.rickjinny;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,6 +17,32 @@ import java.util.List;
 public class T0017_letter_combinations_of_a_phone_number {
 
     public List<String> letterCombinations(String digits) {
-        return null;
+        if (digits == null || digits.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        // 一个映射表，第二个位置是"abc“,第三个位置是"def"。。。
+        // 这里也可以用map，用数组可以更节省点内存
+        String[] letter_map = {
+                " ", "*", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        List<String> res = new ArrayList<>();
+        // 先往队列中加入一个空字符
+        res.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            // 由当前遍历到的字符，取字典表中查找对应的字符串
+            String letters = letter_map[digits.charAt(i) - '0'];
+            int size = res.size();
+            // 计算出队列长度后，将队列中的每个元素挨个拿出来
+            for (int j = 0; j < size; j++) {
+                //每次都从队列中拿出第一个元素
+                String tmp = res.remove(0);
+                //然后跟"def"这样的字符串拼接，并再次放到队列中
+                for (int k = 0; k < letters.length(); k++) {
+                    res.add(tmp + letters.charAt(k));
+                }
+            }
+        }
+        return res;
     }
 }
