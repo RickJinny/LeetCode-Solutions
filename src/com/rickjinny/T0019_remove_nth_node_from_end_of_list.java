3b08ce79b2c9d438a6310d82cd855c70199f56ab
RickJinny <rickjinny@126.com>
2020-05-10 23:40:45 +0800

    LeetCode T0019
    LeetCode T0019

---
+++ src/com/rickjinny/T0019_remove_nth_node_from_end_of_list.java
@@ -0,0 +1,26 @@
+package com.rickjinny;
+
+/**
+ * 19、删除链表的倒数第 N 个节点
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ *
+ * 说明: 给定的 n 保证是有效的。
+ */
+public class T0019_remove_nth_node_from_end_of_list {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+    
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
