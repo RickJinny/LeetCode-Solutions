fc0b80a1b61cffbabcb0bddae8812b158d10d584
RickJinny <rickjinny@126.com>
2020-05-13 23:45:02 +0800

    LeetCode T0020
    LeetCode T0020

--- src/com/rickjinny/T0020_valid_parentheses.java
+++ src/com/rickjinny/T0020_valid_parentheses.java
@@ -1,5 +1,7 @@
 package com.rickjinny;
 
+import java.util.Stack;
+
 /**
  * 20、有效的括号
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
@@ -34,6 +36,32 @@ package com.rickjinny;
 public class T0020_valid_parentheses {
 
     public boolean isValid(String s) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+        char [] arr = s.toCharArray();
+        for (int i =0; i < arr.length; i++) {
+            //如果是开放括号就直接压栈
+            if ( arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
+                stack.push(arr[i]);
+            }else{
+                //特判
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                //获取当前栈顶的元素
+                char topChar = stack.peek();
+                //判断是否为匹配的闭合括号，如果都不是，闭合括号压栈
+                if (arr[i] == ')' && topChar == '(') {
+                    stack.pop();
+                } else if ( arr[i] == '}' && topChar == '{'){
+                    stack.pop();
+                } else if ( arr[i] == ']' && topChar == '['){
+                    stack.pop();
+                } else {
+                    stack.push(arr[i]);
+                }
+            }
+        }
+        //如果栈为空，表示所有括号都匹配成功，返回true，否则返回false
+        return stack.isEmpty();
     }
 }
