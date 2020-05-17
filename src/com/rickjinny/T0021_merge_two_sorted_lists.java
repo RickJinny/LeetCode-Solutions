8f85ba9e8f20af420d73fe28f92184e59a98c570
RickJinny <rickjinny@126.com>
2020-05-17 19:58:25 +0800

    update
    update

--- src/com/rickjinny/T0021_merge_two_sorted_lists.java
+++ src/com/rickjinny/T0021_merge_two_sorted_lists.java
@@ -12,7 +12,17 @@ package com.rickjinny;
 public class T0021_merge_two_sorted_lists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return null;
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
     }
 
     public class ListNode {
