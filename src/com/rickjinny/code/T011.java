25f22a55b7e95e6eceea37761c763696b267d389
RickJinny <rickjinny@126.com>
2020-05-25 23:45:42 +0800

    update
    update

---
+++ src/com/rickjinny/code/T011.java
@@ -0,0 +1,12 @@
+package com.rickjinny.code;
+
+/**
+ * 11、Container With Most Water
+ * 题意：数组下标代表横坐标，数组中的值代表纵坐标，求最大面积
+ * 分类：Array, Two Pointers
+ * Tips：复杂度可以为O(N), 指针往里走, 若值也小了，则面积一定不会增大。和lc42做比较
+ */
+public class T011 {
+
+    
+}
