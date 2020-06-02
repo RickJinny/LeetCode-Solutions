d44ae5c31dab4b58d60ad674dde02610773b51cc
RickJinny <rickjinny@126.com>
2020-06-02 23:36:36 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0026_remove_duplicates_from_sorted_array.java
+++ src/com/rickjinny/leetcode/T0026_remove_duplicates_from_sorted_array.java
@@ -29,7 +29,7 @@ package com.rickjinny.leetcode;
  */
 public class T0026_remove_duplicates_from_sorted_array {
 
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
@@ -41,5 +41,10 @@ public class T0026_remove_duplicates_from_sorted_array {
             }
         }
         return k + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(nums));
     }
 }
