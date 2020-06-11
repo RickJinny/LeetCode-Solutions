0e8b72f4477e7ee9b8a0b4cda4d465017c76b75d
RickJinny <rickjinny@126.com>
2020-06-11 16:04:49 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0029_divide_two_integers.java
+++ src/com/rickjinny/leetcode/T0029_divide_two_integers.java
@@ -6,6 +6,27 @@ package com.rickjinny.leetcode;
 public class T0029_divide_two_integers {
 
     public int divide(int dividend, int divisor) {
-        return 0;
+        boolean sign = (dividend > 0) ^ (divisor > 0);
+        int result = 0;
+        if (dividend > 0) {
+            dividend = -dividend;
+        }
+        if(divisor>0) divisor = -divisor;
+        while(dividend <= divisor) {
+            int temp_result = -1;
+            int temp_divisor = divisor;
+            while(dividend <= (temp_divisor << 1)) {
+                if(temp_divisor <= (Integer.MIN_VALUE >> 1))break;
+                temp_result = temp_result << 1;
+                temp_divisor = temp_divisor << 1;
+            }
+            dividend = dividend - temp_divisor;
+            result += temp_result;
+        }
+        if(!sign) {
+            if(result <= Integer.MIN_VALUE) return Integer.MAX_VALUE;
+            result = - result;
+        }
+        return result;
     }
 }
