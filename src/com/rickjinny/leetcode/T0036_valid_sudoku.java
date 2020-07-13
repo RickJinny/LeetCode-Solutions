28b0a854926d4a5a450e3ee86a90c842f8478200
RickJinny <rickjinny@126.com>
2020-07-13 23:55:55 +0800

    update
    update

---
+++ src/com/rickjinny/leetcode/T0036_valid_sudoku.java
@@ -0,0 +1,12 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 36、有效的数独
+ *
+ */
+public class T0036_valid_sudoku {
+
+    public boolean isValidSudoku(char[][] board) {
+        return false;
+    }
+}
