2a0553b93e0d86b73d411ce0ec9d416ebf55d415
RickJinny <rickjinny@126.com>
2020-08-13 23:36:02 +0800

    search insert
    search insert

--- src/com/rickjinny/leetcode/T0035_search_insert_position.java
+++ src/com/rickjinny/leetcode/T0035_search_insert_position.java
@@ -2,7 +2,8 @@ package com.rickjinny.leetcode;
 
 /**
  * 35. 搜索插入位置
- * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+ * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
  * 你可以假设数组中无重复元素。
  *
  * 示例 1:
@@ -23,7 +24,26 @@ package com.rickjinny.leetcode;
  */
 public class T0035_search_insert_position {
 
-    public int searchInsert(int[] nums, int target) {
-        return 0;
+    public int searchInsert1(int[] nums, int target) {
+        int length = nums.length;
+        if (length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        // 因为有可能数组的最后一个元素的位置的下一个是我们要找的，故右边界是 length
+        int right = length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            // 小于 target 的元素一定不是解
+            if (nums[mid] < target) {
+                // 下一轮搜索的区间是 [mid + 1, right]
+                left = mid + 1;
+            } else {
+                // 下一轮搜索的区间是 [left, mid]
+                right = mid;
+            }
+        }
+        return left;
     }
 }
