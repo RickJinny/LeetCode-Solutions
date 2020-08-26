1890b32c4198f1257b56fb7f4f261f030c6c39d4
RickJinny <rickjinny@126.com>
2020-08-26 23:41:13 +0800

    count and say
    count and say

--- src/com/rickjinny/leetcode/T0038_count_and_say.java
+++ src/com/rickjinny/leetcode/T0038_count_and_say.java
@@ -27,6 +27,34 @@ package com.rickjinny.leetcode;
  */
 public class T0038_count_and_say {
 
+    public String countAndSay(int n) {
+        // 递归终止条件
+        if (n == 1) {
+            return "1";
+        }
+
+        StringBuffer res = new StringBuffer();
+        // 拿到上一层的字符串
+        String str = countAndSay(n - 1);
+        int length = str.length();
+        // 开始指针为0
+        int start = 0;
+        // 注意这从起始条件要和下面长度统一
+        for (int i = 1; i < length + 1; i++) {
+            // 字符串最后一位直接拼接
+            if (i == length) {
+                res.append(i - start)
+                   .append(str.charAt(start));
+            // 直到 start 位的字符串和 i 位的字符串不同，拼接并更新 start 位
+            } else if (str.charAt(i) != str.charAt(start)) {
+                res.append(i - start)
+                   .append(str.charAt(start));
+                start = i;
+            }
+        }
+        return res.toString();
+    }
+
     public static void main(String[] args) {
 
     }
