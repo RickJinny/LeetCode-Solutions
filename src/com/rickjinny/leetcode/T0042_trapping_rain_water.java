44796fab278110fa819e7daac1e9c7edadf98f6f
RickJinny <rickjinny@126.com>
2020-09-10 23:41:23 +0800

    leetcode T0042
    leetcode T0042

---
+++ src/com/rickjinny/leetcode/T0042_trapping_rain_water.java
@@ -0,0 +1,13 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 42、接雨水
+ * 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出: 6
+ */
+public class T0042_trapping_rain_water {
+
+    public int trap(int[] height) {
+        return 0;
+    }
+}
