26552132607536c72f229c436151cc15a5813b66
RickJinny <rickjinny@126.com>
2020-09-11 21:24:26 +0800

    Leetcode 41
    Leetcode 41

--- src/com/rickjinny/leetcode/T0041_first_missing_positive.java
+++ src/com/rickjinny/leetcode/T0041_first_missing_positive.java
@@ -19,6 +19,24 @@ package com.rickjinny.leetcode;
 public class T0041_first_missing_positive {
 
     public int firstMissingPositive(int[] nums) {
-        return 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 }
