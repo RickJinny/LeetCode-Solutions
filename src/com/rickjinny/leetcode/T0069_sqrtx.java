b5d5a22ec0067380b2576e6946332f281d4bec87
RickJinny <rickjinny@126.com>
2020-10-23 23:31:45 +0800

    Leetcode 69
    Leetcode 69

--- src/com/rickjinny/leetcode/T0069_sqrtx.java
+++ src/com/rickjinny/leetcode/T0069_sqrtx.java
@@ -3,6 +3,19 @@ package com.rickjinny.leetcode;
 public class T0069_sqrtx {
 
     public int mySqrt(int x) {
-        return 0;
+        int left = 0;
+        int right = x;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
     }
 }
