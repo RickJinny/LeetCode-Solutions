9edcb9589555d7e70ff3fc151d105f197f15a9df
RickJinny <rickjinny@126.com>
2020-11-01 16:23:55 +0800

    LeetCode 0083
    LeetCode 0083

---
+++ src/com/rickjinny/leetcode/T0083_remove_duplicates_from_sorted_list.java
@@ -0,0 +1,29 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 83、删除排序链表中的重复元素
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 示例1：
+ * 输入：1 -> 1 -> 2
+ * 输出：1 -> 2
+ *
+ * 示例2：
+ * 输入：1 -> 1 -> 2 -> 3 -> 3
+ * 输出：1 -> 2 -> 3
+ */
+public class T0083_remove_duplicates_from_sorted_list {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
