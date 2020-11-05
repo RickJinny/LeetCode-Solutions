192bd078e0a3022cd344df9e714a020a60d43866
RickJinny <rickjinny@126.com>
2020-11-06 01:09:35 +0800

    update 0088
    update 0088

--- src/com/rickjinny/leetcode/T0088_merge_sorted_array.java
+++ src/com/rickjinny/leetcode/T0088_merge_sorted_array.java
@@ -1,4 +1,20 @@
 package com.rickjinny.leetcode;
 
+/**
+ * 88、合并两个有序数组
+ * 给你两个有序整数数组 num1 和 num2, 请你将 num2 合并到 nums1 中，使 num1 成为一个有序数组。
+ * 说明：
+ * 1、初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ * 2、你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 num2 中的元素。
+ *
+ * 示例：
+ * 输入：nums1 = [1, 2, 3, 0, 0 , 0], m = 3
+ *      nums2 = [2, 5, 6], n = 3
+ * 输出：[1, 2, 2, 3, 5, 6]
+ */
 public class T0088_merge_sorted_array {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
 }
