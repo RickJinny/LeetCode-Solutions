48dc298174905b9d29df3266835466a552701ad5
RickJinny <rickjinny@126.com>
2020-11-08 00:07:58 +0800

    T101
    T101

---
+++ src/com/rickjinny/leetcode/T0101_symmetric_tree.java
@@ -0,0 +1,23 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 101、对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * 例如：二叉树 [1, 2, 2, 3, 4, 4, 3] 是对称的。
+ */
+public class T0101_symmetric_tree {
+
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+    
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
