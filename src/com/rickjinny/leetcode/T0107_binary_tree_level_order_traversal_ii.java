9a15b571bf8f886ee4c3611d336d92e89d3dd792
RickJinny <rickjinny@126.com>
2020-11-11 23:56:34 +0800

    107
    107

---
+++ src/com/rickjinny/leetcode/T0107_binary_tree_level_order_traversal_ii.java
@@ -0,0 +1,16 @@
+package com.rickjinny.leetcode;
+
+import java.util.List;
+
+/**
+ * 107、二叉树的层次遍历(二)
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。（即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）。
+ *
+ * 例如：给定二叉树 [3, 9, 20, null, null, 15, 7]
+ */
+public class T0107_binary_tree_level_order_traversal_ii {
+
+    public List<List<Integer>> levelOrderBottom() {
+        return null;
+    }
+}
