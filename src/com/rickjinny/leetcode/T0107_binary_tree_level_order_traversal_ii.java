7f82902ae7f2641f6a14313c37c6a6dea64b14f4
RickJinny <rickjinny@126.com>
2020-11-12 00:10:52 +0800

    update 107
    update 107

--- src/com/rickjinny/leetcode/T0107_binary_tree_level_order_traversal_ii.java
+++ src/com/rickjinny/leetcode/T0107_binary_tree_level_order_traversal_ii.java
@@ -1,6 +1,9 @@
 package com.rickjinny.leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 107、二叉树的层次遍历(二)
@@ -23,7 +26,31 @@ import java.util.List;
 public class T0107_binary_tree_level_order_traversal_ii {
 
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        return null;
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> oneLevel = new ArrayList<>();
+            // 每次都取出一层的所有数据
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                oneLevel.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                // 每次都往队头塞
+                result.addFirst(oneLevel);
+            }
+        }
+        return result;
     }
 
     private static class TreeNode {
