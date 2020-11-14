f46d683d9fdf9f04934d6862d29ca710b1bb036c
RickJinny <rickjinny@126.com>
2020-11-14 23:15:37 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0108_convert_sorted_array_to_binary_search_tree.java
+++ src/com/rickjinny/leetcode/T0108_convert_sorted_array_to_binary_search_tree.java
@@ -16,7 +16,20 @@ package com.rickjinny.leetcode;
 public class T0108_convert_sorted_array_to_binary_search_tree {
 
     public TreeNode sortedArrayToBST(int[] nums) {
-        return null;
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        // 总是选择中间位置左边的数字作为根节点
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
     }
 
     private static class TreeNode {
