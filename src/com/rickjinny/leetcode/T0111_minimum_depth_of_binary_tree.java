5bd116ed96c36e61c0a197cd9a264ba0b65a7eec
RickJinny <rickjinny@126.com>
2020-11-19 00:09:10 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0111_minimum_depth_of_binary_tree.java
+++ src/com/rickjinny/leetcode/T0111_minimum_depth_of_binary_tree.java
@@ -9,7 +9,22 @@ package com.rickjinny.leetcode;
 public class T0111_minimum_depth_of_binary_tree {
 
     public int minDepth(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int min_depth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min_depth = Math.min(minDepth(root.left), min_depth);
+        }
+        if (root.right != null) {
+            min_depth = Math.min(minDepth(root.right), min_depth);
+        }
+        return min_depth + 1;
     }
 
     private static class TreeNode {
@@ -27,6 +42,7 @@ public class T0111_minimum_depth_of_binary_tree {
             this.val = val;
             this.left = left;
             this.right = right;
+
         }
     }
 }
