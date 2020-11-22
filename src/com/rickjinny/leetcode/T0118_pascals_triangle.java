036c402932423cb4f0374e146c1c307b5eb01759
RickJinny <rickjinny@126.com>
2020-11-22 17:57:13 +0800

    LeetCode 118
    LeetCode 118

---
+++ src/com/rickjinny/leetcode/T0118_pascals_triangle.java
@@ -0,0 +1,15 @@
+package com.rickjinny.leetcode;
+
+import java.util.List;
+
+/**
+ * 118、杨辉三角
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ */
+public class T0118_pascals_triangle {
+
+    public List<List<Integer>> generate(int numRows) {
+        return null;
+    }
+}
