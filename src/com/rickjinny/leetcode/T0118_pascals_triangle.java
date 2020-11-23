22a213528e0d16593bcabe05f17d3175867aa30c
RickJinny <rickjinny@126.com>
2020-11-23 23:54:36 +0800

    update 118
    update 118

--- src/com/rickjinny/leetcode/T0118_pascals_triangle.java
+++ src/com/rickjinny/leetcode/T0118_pascals_triangle.java
@@ -1,5 +1,6 @@
 package com.rickjinny.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,6 +11,20 @@ import java.util.List;
 public class T0118_pascals_triangle {
 
     public List<List<Integer>> generate(int numRows) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> subList = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                // 第一个位置和最后一个位置的元素为1
+                if (j == 0 || j == i) {
+                    subList.add(1);
+                } else {
+                    // 上一行的元素进行相加
+                    subList.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(i));
+                }
+            }
+            result.add(subList);
+        }
+        return result;
     }
 }
