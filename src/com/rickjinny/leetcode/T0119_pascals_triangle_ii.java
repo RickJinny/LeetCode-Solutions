77f6f8f22c0a8e92bc9eb2a2ba3f319d9c93bce5
RickJinny <rickjinny@126.com>
2020-11-26 00:38:00 +0800

    LeetCode 119
    LeetCode 119

---
+++ src/com/rickjinny/leetcode/T0119_pascals_triangle_ii.java
@@ -0,0 +1,19 @@
+package com.rickjinny.leetcode;
+
+import java.util.List;
+
+/**
+ * 119、杨辉三角 II
+ * 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+ *
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * 示例：
+ * 输入：3
+ * 输出：[1, 3, 3, 1]
+ */
+public class T0119_pascals_triangle_ii {
+
+    public List<Integer> getRow(int rowIndex) {
+        return null;
+    }
+}
