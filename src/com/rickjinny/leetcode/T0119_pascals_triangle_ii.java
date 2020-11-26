2809fd38cca6938b6755884586f8b4c09ace44c8
RickJinny <rickjinny@126.com>
2020-11-26 23:38:14 +0800

    LeetCode 119
    LeetCode 119

--- src/com/rickjinny/leetcode/T0119_pascals_triangle_ii.java
+++ src/com/rickjinny/leetcode/T0119_pascals_triangle_ii.java
@@ -1,5 +1,6 @@
 package com.rickjinny.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,6 +15,12 @@ import java.util.List;
 public class T0119_pascals_triangle_ii {
 
     public List<Integer> getRow(int rowIndex) {
-        return null;
+        List<Integer> res = new ArrayList<>(rowIndex + 1);
+        long cur = 1;
+        for (int i = 0; i <= rowIndex; i++) {
+            res.add((int) cur);
+            cur = cur * (rowIndex - i) / (i + 1);
+        }
+        return res;
     }
 }
