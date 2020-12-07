3553fceae321f57f5b378dac7d44ddf0d18b9b08
RickJinny <rickjinny@126.com>
2020-12-07 23:14:06 +0800

    linked list cycle
    linked list cycle

--- src/com/rickjinny/leetcode/T0141_linked_list_cycle.java
+++ src/com/rickjinny/leetcode/T0141_linked_list_cycle.java
@@ -35,10 +35,23 @@ package com.rickjinny.leetcode;
 public class T0141_linked_list_cycle {
 
     public boolean hasCycle(ListNode head) {
-        return false;
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 
-    private class ListNode {
+    private static class ListNode {
         int val;
         ListNode next;
 
