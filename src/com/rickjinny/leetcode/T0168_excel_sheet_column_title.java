df642dda1f2f5dd44a2c6a9f55d02d552bc21769
RickJinny <rickjinny@126.com>
2020-12-12 00:36:18 +0800

    LeetCode 168
    LeetCode 168

---
+++ src/com/rickjinny/leetcode/T0168_excel_sheet_column_title.java
@@ -0,0 +1,34 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 168、Excel 表列名称
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ *
+ * 例如:
+ *      1 -> A
+ *      2 -> B
+ *      3 -> C
+ *      ...
+ *      26 -> Z
+ *      27 -> AA
+ *      28 -> AB
+ *      ...
+ *
+ * 示例1:
+ *   输入: 1
+ *   输出: "A"
+ *
+ * 示例2:
+ *   输入: 28
+ *   输出: "AB"
+ *
+ * 示例3:
+ *   输入: 701
+ *   输出: "ZY"
+ */
+public class T0168_excel_sheet_column_title {
+
+    public String convertToTitle(int n) {
+        return null;
+    }
+}
