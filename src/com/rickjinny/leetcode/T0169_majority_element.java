875f4bd7a36edfddd244f72131f942a840a97cad
RickJinny <rickjinny@126.com>
2020-12-13 01:22:56 +0800

    LeetCode 169
    LeetCode 169

--- src/com/rickjinny/leetcode/T0169_majority_element.java
+++ src/com/rickjinny/leetcode/T0169_majority_element.java
@@ -1,5 +1,8 @@
 package com.rickjinny.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 169、多数元素
  * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 n/2 的元素。
@@ -16,6 +19,25 @@ package com.rickjinny.leetcode;
 public class T0169_majority_element {
 
     public int majorityElement(int[] nums) {
-        return 0;
+        Map<Integer, Integer> counts = countNums(nums);
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+        return majorityEntry.getKey();
+    }
+
+    private Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int num : nums) {
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
+            } else {
+                counts.put(num, counts.get(num) + 1);
+            }
+        }
+        return counts;
     }
 }
