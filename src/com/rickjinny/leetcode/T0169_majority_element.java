c586657ff65bc133809a43961bcc579839d95896
RickJinny <rickjinny@126.com>
2020-12-12 19:24:33 +0800

    LeetCode 169
    LeetCode 169

---
+++ src/com/rickjinny/leetcode/T0169_majority_element.java
@@ -0,0 +1,21 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 169、多数元素
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 n/2 的元素。
+ * 你可以假定数组是非空的，并且给定的数组总数存在多数元素。
+ *
+ * 示例1:
+ * 输入: [3, 2, 3]
+ * 输出: 3
+ *
+ * 示例2:
+ * 输入: [2, 2, 1, 1, 1, 2, 2]
+ * 输出: 2
+ */
+public class T0169_majority_element {
+
+    public int majorityElement(int[] nums) {
+        return 0;
+    }
+}
