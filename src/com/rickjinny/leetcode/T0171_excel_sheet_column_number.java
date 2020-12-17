34d88047122ed465f125bbdc8d512cb2a5980dd7
RickJinny <rickjinny@126.com>
2020-12-18 00:00:55 +0800

    update 171
    update 171

--- src/com/rickjinny/leetcode/T0171_excel_sheet_column_number.java
+++ src/com/rickjinny/leetcode/T0171_excel_sheet_column_number.java
@@ -3,9 +3,10 @@ package com.rickjinny.leetcode;
 /**
  * 171、Excel表列序号
  * 给定一个 Excel 表格中的列名称，返回其相应的序列号。
- * 例如：
- *
  */
 public class T0171_excel_sheet_column_number {
 
+    public int titleToNumber(String s) {
+        return 0;
+    }
 }
