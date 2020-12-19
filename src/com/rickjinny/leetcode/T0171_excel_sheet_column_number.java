dfe0b26f3326d9abd9c3cc96311786ea8398f467
RickJinny <rickjinny@126.com>
2020-12-19 16:41:25 +0800

    LeetCode 0171
    LeetCode 0171

--- src/com/rickjinny/leetcode/T0171_excel_sheet_column_number.java
+++ src/com/rickjinny/leetcode/T0171_excel_sheet_column_number.java
@@ -1,12 +1,40 @@
 package com.rickjinny.leetcode;
 
 /**
- * 171、Excel表列序号
+ * 171、Excel表列序号。
  * 给定一个 Excel 表格中的列名称，返回其相应的序列号。
+ *
+ * 例如:
+ *      A -> 1
+ *      B -> 2
+ *      C -> 3
+ *      ...
+ *      Z -> 26
+ *      AA -> 27
+ *      AB -> 28
+ *      ...
+ *
+ * 示例1:
+ *   输入: "A"
+ *   输出: 1
+ *
+ * 示例2:
+ *   输入: "AB"
+ *   输出: 28
+ *
+ * 示例3:
+ *   输入: "ZY"
+ *   输出: 701
+ *
  */
 public class T0171_excel_sheet_column_number {
 
     public int titleToNumber(String s) {
-        return 0;
+        char[] charArray = s.toCharArray();
+        int res = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            res = res * 26 + (charArray[i] - 'A' + 1);
+        }
+        return res;
     }
 }
