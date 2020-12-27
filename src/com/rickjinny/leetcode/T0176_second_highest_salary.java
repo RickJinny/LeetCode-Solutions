fafd012dadf1616177e495a578101f0cabb179b5
RickJinny <rickjinny@126.com>
2020-12-27 19:14:41 +0800

    LeetCode 176
    LeetCode 176

---
+++ src/com/rickjinny/leetcode/T0176_second_highest_salary.java
@@ -0,0 +1,27 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 176、第二高的薪水
+ * 编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary）。
+ * +-----+---------+
+ * | Id  |  Salary |
+ * +-----+---------+
+ * |  1  |  100    |
+ * |  2  |  200    |
+ * |  3  |  300    |
+ * +-----+---------+
+ *
+ * 例如上述 Employee 表，SQL 查询应该返回 200 作为第二高的薪水。
+ * 如果不存在第二高的薪水，那么查询应返回 null。
+ * +-----------------------+
+ * |  SecondHighestSalary  |
+ * +-----------------------+
+ * |         200           |
+ * +-----------------------+
+ */
+public class T0176_second_highest_salary {
+
+    public static void main(String[] args) {
+        
+    }
+}
