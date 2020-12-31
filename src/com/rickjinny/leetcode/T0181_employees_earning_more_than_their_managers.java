ac62de0e6459e8a2e54ec4c99ed58bb2a3995292
RickJinny <rickjinny@126.com>
2021-01-01 00:49:48 +0800

    Leetcode 0181
    Leetcode 0181

--- src/com/rickjinny/leetcode/T0181_employees_earning_more_than_their_managers.java
+++ src/com/rickjinny/leetcode/T0181_employees_earning_more_than_their_managers.java
@@ -6,7 +6,6 @@ package com.rickjinny.leetcode;
 public class T0181_employees_earning_more_than_their_managers {
 
     public static void main(String[] args) {
-
+        System.out.println("select a.Name as 'Employee' from Employee as a, Employee as b where a.ManagerId = b.Id and a.Salary > b.Salary;");
     }
-
 }
