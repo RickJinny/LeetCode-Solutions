5bc0ea5ba30d62b4708f1325514689b561c9743e
RickJinny <rickjinny@126.com>
2021-01-06 00:46:45 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0196_delete_duplicate_emails.java
+++ src/com/rickjinny/leetcode/T0196_delete_duplicate_emails.java
@@ -6,6 +6,12 @@ package com.rickjinny.leetcode;
 public class T0196_delete_duplicate_emails {
 
     public static void main(String[] args) {
-
+        /**
+         * 先查询，再删除
+         * (1) 查询
+         * select p1.* from Person p1, Person p2 where p1.Email = p2.Email and p1.Id > p2.Id;
+         * (2) 删除
+         * delete p1 from Person p1, Person p2 where p1.Email = p2.Email and p1.Id > p2.Id;
+         */
     }
 }
