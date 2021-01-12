89e500ff781b67a9d41614a487397fd029c3bbb3
RickJinny <rickjinny@126.com>
2021-01-12 23:28:07 +0800

    203
    203

---
+++ src/com/rickjinny/leetcode/T0203_remove_linked_list_elements.java
@@ -0,0 +1,26 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 203、移除链表元素
+ * 删除链表中等于给定值 val 的所有节点
+ *
+ * 示例:
+ * 输入: 1 -> 2 -> 6 -> 3 -> 4 -> 5 -> 6, val = 6
+ * 输出: 1 —> 2 -> 3 -> 4 -> 5
+ */
+public class T0203_remove_linked_list_elements {
+
+    public ListNode removeElements(ListNode head, int val) {
+        
+        return null;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
