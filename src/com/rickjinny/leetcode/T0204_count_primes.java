de406cad96ba4df1199009b61b73448bab66059d
RickJinny <rickjinny@126.com>
2021-01-14 23:40:42 +0800

    update
    update

---
+++ src/com/rickjinny/leetcode/T0204_count_primes.java
@@ -0,0 +1,27 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 204、计数质数: 统计所有小于非负数 n 的质数的数量。
+ *
+ * 示例1:
+ * 输入: n = 10
+ * 输出: 4
+ * 解释: 小于 10 的质数一共有 4 个，它们是 2, 3, 5, 7
+ *
+ * 示例2:
+ * 输入: n = 0
+ * 输出: 0
+ *
+ * 示例3:
+ * 输入: n = 1
+ * 输出: 0
+ *
+ * 提示: 0 <= n <= 5 * 10 ^ 6
+ *
+ */
+public class T0204_count_primes {
+
+    public int countPrimes(int n) {
+        return 0;
+    }
+}
