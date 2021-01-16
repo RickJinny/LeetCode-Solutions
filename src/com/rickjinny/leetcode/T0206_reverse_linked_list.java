234917b98e7fe0dc831299027209d5f9cf611133
RickJinny <rickjinny@126.com>
2021-01-16 11:06:43 +0800

    LeetCode 206
    LeetCode 206

---
+++ src/com/rickjinny/leetcode/T0206_reverse_linked_list.java
@@ -0,0 +1,28 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 206、反转链表
+ * 反转一个单链表。
+ *
+ * 示例:
+ * 输入: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+ * 输出: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+ *
+ * 进阶：你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ */
+public class T0206_reverse_linked_list {
+
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
