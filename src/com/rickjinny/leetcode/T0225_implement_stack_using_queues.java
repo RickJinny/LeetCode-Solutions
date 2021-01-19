e20cc29d3cbd5787b31b816aaf51417032c2635e
RickJinny <rickjinny@126.com>
2021-01-19 23:14:57 +0800

    LeetCode 225
    LeetCode 225

--- src/com/rickjinny/leetcode/T0225_implement_stack_using_queues.java
+++ src/com/rickjinny/leetcode/T0225_implement_stack_using_queues.java
@@ -1,5 +1,8 @@
 package com.rickjinny.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 225、用队列实现栈
  * 使用队列实现栈的下列操作:
@@ -20,24 +23,43 @@ public class T0225_implement_stack_using_queues {
 
     private static class MyStack {
 
+        // 初始化队列
+        Queue<Integer> queue;
+
         public MyStack() {
-
+            queue = new LinkedList<>();
         }
 
+        /**
+         * 模拟入栈操作
+         */
         public void push(int x) {
-
+            queue.offer(x);
+            // 将之前的全部都出队，然后再入队
+            for (int i = 0; i < queue.size(); i++) {
+                queue.offer(queue.poll());
+            }
         }
 
+        /**
+         * 模拟出栈
+         */
         public int pop() {
-            return 0;
+            return queue.poll();
         }
 
+        /**
+         * 返回栈顶元素
+         */
         public int top() {
-            return 0;
+            return queue.peek();
         }
 
+        /**
+         * 判断是否为空
+         */
         public boolean empty() {
-            return false;
+            return queue.isEmpty();
         }
     }
 }
