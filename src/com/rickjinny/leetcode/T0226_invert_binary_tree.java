d00253b72ac9748bd35e8baa4b5acba7b576adea
RickJinny <rickjinny@126.com>
2021-01-20 23:47:04 +0800

    LeetCode 0226
    LeetCode 0226

---
+++ src/com/rickjinny/leetcode/T0226_invert_binary_tree.java
@@ -0,0 +1,47 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 226、翻转二叉树
+ * 翻转一棵二叉树
+ *
+ * 示例:
+ *
+ * 输入:
+ *        4
+ *     /    \
+ *    2     7
+ *   / |   / |
+ *  1  2  6  9
+ *
+ *
+ * 输出:
+ *        4
+ *     /    \
+ *    7     2
+ *   / |   / |
+ *  9  6  3  1
+ *
+ */
+public class T0226_invert_binary_tree {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
