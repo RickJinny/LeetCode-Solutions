0e4c9ce12f841e6aee0f1a1afbf25d67ec41cb29
RickJinny <rickjinny@126.com>
2021-01-31 23:36:49 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0242_valid_anagram.java
+++ src/com/rickjinny/leetcode/T0242_valid_anagram.java
@@ -1,5 +1,8 @@
 package com.rickjinny.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 242、有效的字母异位词
  * 给定两个字符串 s 和 t，编写一个函数来判断 t 是否是 s 的字母异位词。
@@ -18,6 +21,23 @@ package com.rickjinny.leetcode;
 public class T0242_valid_anagram {
 
     public boolean isAnagram(String s, String t) {
-        return false;
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) - 1);
+            if (map.get(ch) < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
