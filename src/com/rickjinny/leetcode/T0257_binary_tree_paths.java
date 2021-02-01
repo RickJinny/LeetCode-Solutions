afdd0b68381f0899a98fa5802c3eb7787802b056
RickJinny <rickjinny@126.com>
2021-02-01 23:16:52 +0800

    update 257
    update 257

--- src/com/rickjinny/leetcode/T0257_binary_tree_paths.java
+++ src/com/rickjinny/leetcode/T0257_binary_tree_paths.java
@@ -1,5 +1,7 @@
 package com.rickjinny.leetcode;
 
+import java.util.List;
+
 /**
  * 257、二叉树的所有路径
  * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
@@ -17,6 +19,27 @@ package com.rickjinny.leetcode;
  */
 public class T0257_binary_tree_paths {
 
-    
+    public List<String> binaryTreePaths(TreeNode root) {
+        return null;
+    }
 
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
