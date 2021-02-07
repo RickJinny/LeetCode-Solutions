30eda73156571d3c7f393224cbd5fd228dd9400a
RickJinny <rickjinny@126.com>
2021-02-07 23:47:56 +0800

    update0283
    update0283

--- src/com/rickjinny/leetcode/T0283_move_zeroes.java
+++ src/com/rickjinny/leetcode/T0283_move_zeroes.java
@@ -14,6 +14,21 @@ package com.rickjinny.leetcode;
 public class T0283_move_zeroes {
 
     public void moveZeros(int[] nums) {
-        
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
