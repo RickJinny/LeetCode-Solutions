0b74c301dd807d5f7675e40ba38017e8f96a4e95
RickJinny <rickjinny@126.com>
2021-02-23 23:34:27 +0800

    update
    update

---
+++ src/com/rickjinny/leetcode/T0345_reverse_vowels_of_a_string.java
@@ -0,0 +1,22 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 345、反转字符串中的元音字母
+ * 编写一个函数，以字符串作为输入, 反转该字符串中的元音字母。
+ *
+ * 示例1:
+ * 输入: "hello"
+ * 输出: "holle"
+ *
+ * 示例2:
+ * 输入: "leetcode"
+ * 输出: "leotcede"
+ *
+ * 提示: 元音字母不包含字母 "y"。
+ */
+public class T0345_reverse_vowels_of_a_string {
+
+    public String reverseVowels(String s) {
+        return null;
+    }
+}
