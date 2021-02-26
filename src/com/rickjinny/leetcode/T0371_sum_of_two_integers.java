ff9768cb89adf77cdb44c6ef4baf3c0492bbd04e
RickJinny <rickjinny@126.com>
2021-02-26 23:55:37 +0800

    LeetCode0371
    LeetCode0371

---
+++ src/com/rickjinny/leetcode/T0371_sum_of_two_integers.java
@@ -0,0 +1,20 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 371、两整数之后
+ * 不使用运算符 + 和 -，计算两整数 a、b 之和。
+ *
+ * 示例1:
+ * 输入: a = 1, b = 2
+ * 输出: 3
+ *
+ * 示例2:
+ * 输入: a = -2, b = 3
+ * 输出: 1
+ */
+public class T0371_sum_of_two_integers {
+
+    public int getSum(int a, int b) {
+        return 0;
+    }
+}
