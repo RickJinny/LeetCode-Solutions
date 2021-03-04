c6c49b5941206c25284683322101480ff37f2bb7
RickJinny <rickjinny@126.com>
2021-03-05 00:04:01 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0387_first_unique_character_in_a_string.java
+++ src/com/rickjinny/leetcode/T0387_first_unique_character_in_a_string.java
@@ -1,5 +1,8 @@
 package com.rickjinny.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 387、字符串中的第一个唯一字符
  * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存，则返回 -1。
@@ -16,6 +19,16 @@ package com.rickjinny.leetcode;
 public class T0387_first_unique_character_in_a_string {
 
     public int firstUniqChar(String s) {
-        return 0;
+        Map<Character, Integer> map = new HashMap<>(26);
+        char[] chars = s.toCharArray();
+        for (char ch : chars) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (map.get(chars[i]) == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
