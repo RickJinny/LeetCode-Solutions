f7a65a4f0b34e9458023ad39d4dd953f76b2b3dd
RickJinny <rickjinny@126.com>
2021-03-04 23:59:04 +0800

    LeetCode387
    LeetCode387

---
+++ src/com/rickjinny/leetcode/T0387_first_unique_character_in_a_string.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 387、
+ */
+public class T0387_first_unique_character_in_a_string {
+
+    public int firstUniqChar(String s) {
+        return 0;
+    }
+}
