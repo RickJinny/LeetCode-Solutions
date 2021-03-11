4d2fcadb6d3846baa03c1e89e41044af6c71d225
RickJinny <rickjinny@126.com>
2021-03-11 23:17:21 +0800

    LeetCode0401
    LeetCode0401

---
+++ src/com/rickjinny/leetcode/T401_binary_watch.java
@@ -0,0 +1,19 @@
+package com.rickjinny.leetcode;
+
+import java.util.List;
+
+/**
+ * 401、二进制手表
+ * 二进制手表顶部有 4 个 LED 代表小时 (0-11), 底部的 6 个 LED 代表 分钟 (0-59)
+ * 每个 LED 代表一个 0 或 1，最低位在右侧。
+ *
+ * 示例:
+ * 输入: n = 1
+ * 返回: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+ */
+public class T401_binary_watch {
+
+    public List<String> readBinaryWatch(int num) {
+        return null;
+    }
+}
