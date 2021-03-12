94fe88c0489e647fd1154ea932168ceb37589271
RickJinny <rickjinny@126.com>
2021-03-12 23:44:14 +0800

    update
    update

--- src/com/rickjinny/leetcode/T401_binary_watch.java
+++ src/com/rickjinny/leetcode/T401_binary_watch.java
@@ -1,5 +1,6 @@
 package com.rickjinny.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +14,31 @@ import java.util.List;
  */
 public class T401_binary_watch {
 
+    int[] hours = new int[]{1, 2, 4, 8, 0, 0, 0, 0, 0, 0};
+    int[] minutes = new int[]{0, 0, 0, 0, 1, 2, 4, 8, 16, 32};
+    List<String> res = new ArrayList<>();
+
     public List<String> readBinaryWatch(int num) {
-        return null;
+        backTrack(num, 0, 0, 0);
+        return res;
+    }
+
+    public void backTrack(int num, int index, int hour, int minute) {
+        if (hour > 11 || minute > 59) {
+            return;
+        }
+        if (num == 0) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(hour).append(":");
+            if (minute < 10) {
+                sb.append('0');
+            }
+            sb.append(minute);
+            res.add(sb.toString());
+            return;
+        }
+        for (int i = index; i < 10; i++) {
+            backTrack(num - 1, i + 1, hour + hours[i], minute + minutes[i]);
+        }
     }
 }
