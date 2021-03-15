a311f59a97aaca60b450b34948bf5f1a1a307f26
RickJinny <rickjinny@126.com>
2021-03-15 23:41:35 +0800

    LeetCode0405
    LeetCode0405

---
+++ src/com/rickjinny/leetcode/T0405_convert_a_number_to_hexadecimal.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 405、数字转换为十六进制
+ */
+public class T0405_convert_a_number_to_hexadecimal {
+
+    public String toHex(int num) {
+        return null;
+    }
+}
