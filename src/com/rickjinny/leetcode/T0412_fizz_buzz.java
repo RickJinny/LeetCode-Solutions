d7553b216514dd6d2b4953ed1d7c1934c0536761
RickJinny <rickjinny@126.com>
2021-03-23 23:46:23 +0800

    LeetCode0412
    LeetCode0412

---
+++ src/com/rickjinny/leetcode/T0412_fizz_buzz.java
@@ -0,0 +1,18 @@
+package com.rickjinny.leetcode;
+
+import java.util.List;
+
+/**
+ * 412、Fizz Buzz
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ * (1)、如果 n 是 3 的倍数，输出 "Fizz"；
+ * (2)、如果 n 是 5 的倍数，输出 "Buzz"；
+ * (3)、如果 n 同时是 3 和 5 的倍数，输出 "FizzBuzz"
+ */
+public class T0412_fizz_buzz {
+
+    public List<String> fizzBuzz(int n) {
+        return null;
+    }
+
+}
