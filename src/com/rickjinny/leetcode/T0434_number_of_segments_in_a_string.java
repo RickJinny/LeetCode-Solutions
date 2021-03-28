d829a484956debfa2b57a3bf2226bfe43ff5cc1d
RickJinny <rickjinny@126.com>
2021-03-28 23:31:12 +0800

    LeetCode0434
    LeetCode0434

---
+++ src/com/rickjinny/leetcode/T0434_number_of_segments_in_a_string.java
@@ -0,0 +1,18 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 434、字符串中的单词数
+ * 统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+ * 请注意，你可以假定字符串里不包括任何不可打印的字符。
+ * <p>
+ * 示例：
+ * 输入: "Hello, my name is John"
+ * 输出: 5
+ * 解释: 这里的单词是指连续的不是空格的字符，所以 "Hell0, " 算作 1 个单词。
+ */
+public class T0434_number_of_segments_in_a_string {
+
+    public int countSegments(String s) {
+        return 0;
+    }
+}
