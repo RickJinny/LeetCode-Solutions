67af03f5133573368853e4c2850bca634ee8a8d4
RickJinny <rickjinny@126.com>
2021-03-30 08:01:22 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0441_arranging_coins.java
+++ src/com/rickjinny/leetcode/T0441_arranging_coins.java
@@ -27,7 +27,20 @@ package com.rickjinny.leetcode;
 public class T0441_arranging_coins {
 
     public int arrangeCoins(int n) {
-        return 0;
+        int low = 0;
+        int high = n;
+        while (low < high) {
+            long mid = (high - low) / 2 + low;
+            long cost = ((mid + 1) * mid) / 2;
+            if (cost == n) {
+                return (int) mid;
+            } else if (cost > n) {
+                high = (int) mid - 1;
+            } else {
+                low = (int) mid + 1;
+            }
+        }
+        return high;
     }
 
 }
