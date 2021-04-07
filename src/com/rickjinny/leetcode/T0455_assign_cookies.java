66eee092ddd550cb1db2b6e88595c9cb750907fc
RickJinny <rickjinny@126.com>
2021-04-07 08:26:58 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0455_assign_cookies.java
+++ src/com/rickjinny/leetcode/T0455_assign_cookies.java
@@ -1,11 +1,27 @@
 package com.rickjinny.leetcode;
 
+import java.util.Arrays;
+
 /**
  * 455、分发饼干
  */
 public class T0455_assign_cookies {
 
     public int findContentChildren(int[] g, int[] s) {
-        return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int numOfChildren = g.length;
+        int numOfCookies = s.length;
+        int count = 0;
+        for (int i = 0, j = 0; i < numOfChildren && j < numOfCookies; i++, j++) {
+            while (j < numOfCookies && g[i] > s[j]) {
+                j++;
+            }
+            if (j < numOfCookies) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
