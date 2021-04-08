9494786fb58d868e08aa285c2dda35c5ea37a23d
RickJinny <rickjinny@126.com>
2021-04-08 08:13:53 +0800

    LeetCode0459
    LeetCode0459

---
+++ src/com/rickjinny/leetcode/T0459_repeated_substring_pattern.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 459、重复的子字符串
+ */
+public class T0459_repeated_substring_pattern {
+
+    public boolean repeatedSubstringPattern(String s) {
+        return false;
+    }
+}
