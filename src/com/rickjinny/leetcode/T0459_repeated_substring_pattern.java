a001fab0998be76f5c087525bbbade9ae0e66829
RickJinny <rickjinny@126.com>
2021-04-11 15:27:22 +0800

    459
    459

--- src/com/rickjinny/leetcode/T0459_repeated_substring_pattern.java
+++ src/com/rickjinny/leetcode/T0459_repeated_substring_pattern.java
@@ -22,6 +22,21 @@ package com.rickjinny.leetcode;
 public class T0459_repeated_substring_pattern {
 
     public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i * 2 < n; ++i) {
+            if (n % i == 0) {
+                boolean match = true;
+                for (int j = i; j < n; ++j) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
