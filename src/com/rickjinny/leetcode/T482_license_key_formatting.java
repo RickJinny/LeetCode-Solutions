2bbc5310eb7a242b26938f830edf5ef2edcb977b
RickJinny <rickjinny@126.com>
2021-04-28 23:40:31 +0800

    LeetCode0482
    LeetCode0482

---
+++ src/com/rickjinny/leetcode/T482_license_key_formatting.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 482、密钥格式化
+ */
+public class T482_license_key_formatting {
+
+    public String licenseKeyFormatting(String s, int k) {
+        return "";
+    }
+}
