255f79f357cf167d31393436e6a03cd54522eb53
RickJinny <rickjinny@126.com>
2021-05-05 10:23:16 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0485_max_consecutive_ones.java
+++ src/com/rickjinny/leetcode/T0485_max_consecutive_ones.java
@@ -16,6 +16,18 @@ package com.rickjinny.leetcode;
 public class T0485_max_consecutive_ones {
 
     public int findMaxConsecutiveOnes(int[] nums) {
-        return 0;
+        int maxCount = 0;
+        int count = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                maxCount = Math.max(maxCount, count);
+                count = 0;
+            }
+        }
+        maxCount = Math.max(maxCount, count);
+        return maxCount;
     }
 }
