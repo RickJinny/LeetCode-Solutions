bd31da7497195aef13604f0638dae5b75433ea8d
RickJinny <rickjinny@126.com>
2021-05-08 23:50:46 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0496_next_greater_element_i.java
+++ src/com/rickjinny/leetcode/T0496_next_greater_element_i.java
@@ -1,5 +1,10 @@
 package com.rickjinny.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 496、下一个更大元素
  * 给你两个没有重复元素的数组 nums1 和 nums2，其中 nums1 是 nums2 的子集。
@@ -23,6 +28,24 @@ package com.rickjinny.leetcode;
 public class T0496_next_greater_element_i {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        return null;
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        // 先处理 num2，把对应关系存入哈希表
+        for (int i = 0; i < len2; i++) {
+            while (!stack.isEmpty() && stack.peekLast() < nums2[i]) {
+                map.put(stack.removeLast(), nums2[i]);
+            }
+            stack.addLast(nums2[i]);
+        }
+
+        // 遍历 nums1 得到结果集
+        int[] res = new int[len1];
+        for (int i = 0; i < len1; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
     }
 }
