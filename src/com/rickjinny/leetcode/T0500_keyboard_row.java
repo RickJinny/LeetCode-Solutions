7838fda38eded55b3ee483b281da72a326dcf95b
RickJinny <rickjinny@126.com>
2021-05-09 17:26:45 +0800

    LeetCode0500
    LeetCode0500

---
+++ src/com/rickjinny/leetcode/T0500_keyboard_row.java
@@ -0,0 +1,30 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 500、键盘行
+ * 给你一个字符串数组 words，只返回可以使用在 美式键盘 同一行的字母打印出来的单词。
+ *
+ * 示例1:
+ * 输入: words = ["Hello", "Alaska", "Dad", "Peace"]
+ * 输出: ["Alaska", "Dad"]
+ *
+ * 示例2:
+ * 输入: words = ["omk"]
+ * 输出: []
+ *
+ * 示例3:
+ * 输入: words = ["adsdf", "sfd"]
+ * 输出: ["adsdf", "sfd"]
+ *
+ * 提示:
+ * 1 <= words.length <= 20
+ * 1 <= words[i].length <= 100
+ * words[i] 由英文字母（小写和大写字母）组成
+ *
+ */
+public class T0500_keyboard_row {
+
+    public String[] findWords(String[] words) {
+        return null;
+    }
+}
