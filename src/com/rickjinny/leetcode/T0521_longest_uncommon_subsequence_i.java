21276740dc631a1eefd7799c560f9d7daf76ff82
RickJinny <rickjinny@126.com>
2021-05-21 23:47:09 +0800

    LeetCode0521
    LeetCode0521

---
+++ src/com/rickjinny/leetcode/T0521_longest_uncommon_subsequence_i.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 521、最长特殊序列 I
+ */
+public class T0521_longest_uncommon_subsequence_i {
+
+    public int findLUSLength(String a, String b) {
+        return 0;
+    }
+}
