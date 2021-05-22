79f0d1ed6de52045e8c3787471c1646e00ce97f3
RickJinny <rickjinny@126.com>
2021-05-22 16:54:48 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0521_longest_uncommon_subsequence_i.java
+++ src/com/rickjinny/leetcode/T0521_longest_uncommon_subsequence_i.java
@@ -1,11 +1,16 @@
 package com.rickjinny.leetcode;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
 /**
  * 521、最长特殊序列 I
  */
 public class T0521_longest_uncommon_subsequence_i {
 
     public int findLUSLength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
         return 0;
     }
 }
