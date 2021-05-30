4010f71f4c6ba2fc5c039dd4264f0a635db10c47
RickJinny <rickjinny@126.com>
2021-05-31 00:11:31 +0800

    LeetCode0551
    LeetCode0551

---
+++ src/com/rickjinny/leetcode/T0551_student_attendance_record_i.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 551、学生出勤记录 I
+ */
+public class T0551_student_attendance_record_i {
+
+    public boolean checkRecord(String s) {
+        return false;
+    }
+}
