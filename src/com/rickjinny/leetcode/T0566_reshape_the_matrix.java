c5f98f5adfc587c4b7ad23ffb4ee4dd19fbd287f
RickJinny <rickjinny@126.com>
2021-07-03 23:40:34 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0566_reshape_the_matrix.java
+++ src/com/rickjinny/leetcode/T0566_reshape_the_matrix.java
@@ -16,7 +16,17 @@ package com.rickjinny.leetcode;
  */
 public class T0566_reshape_the_matrix {
 
-    public int[] matrixReshape(int[][] mat, int r, int c) {
-        return null;
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length;
+        int n = nums[0].length;
+        if (m * n != r * c) {
+            return nums;
+        }
+
+        int[][] ans = new int[r][c];
+        for (int x = 0; x < m * n; ++x) {
+            ans[x / c][x % c] = nums[x / n][x % n];
+        }
+        return ans;
     }
 }
