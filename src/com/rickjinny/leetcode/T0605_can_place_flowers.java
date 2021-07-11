3232a495e8ed264d17d29e1b2e74084d2e66e3c2
RickJinny <rickjinny@126.com>
2021-07-11 23:55:59 +0800

    LeetCode0605
    LeetCode0605

---
+++ src/com/rickjinny/leetcode/T0605_can_place_flowers.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 605、种花问题
+ */
+public class T0605_can_place_flowers {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        return false;
+    }
+}
