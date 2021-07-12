e101c12a8c0f0bb72d8b72463e6bd390bf82daa5
RickJinny <rickjinny@126.com>
2021-07-12 23:57:10 +0800

    LeetCode0605
    LeetCode0605

--- src/com/rickjinny/leetcode/T0605_can_place_flowers.java
+++ src/com/rickjinny/leetcode/T0605_can_place_flowers.java
@@ -6,6 +6,26 @@ package com.rickjinny.leetcode;
 public class T0605_can_place_flowers {
 
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        return false;
+        if (flowerbed == null || flowerbed.length == 0) {
+            return n == 0;
+        }
+
+        int countOfZero = 1;
+        int canPlace = 0;
+        for (int bed : flowerbed) {
+            if (bed == 0) {
+                countOfZero++;
+            } else {
+                canPlace += (countOfZero - 1) / 2;
+                if (canPlace >= n) {
+                    return true;
+                }
+                countOfZero = 0;
+            }
+        }
+
+        countOfZero++;
+        canPlace += (countOfZero - 1) / 2;
+        return canPlace >= n;
     }
 }
