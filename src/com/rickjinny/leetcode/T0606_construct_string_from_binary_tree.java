a93505f9d085ab73232825bc9a9d44484088003f
RickJinny <rickjinny@126.com>
2021-07-13 23:24:57 +0800

    LeetCode0606
    LeetCode0606

---
+++ src/com/rickjinny/leetcode/T0606_construct_string_from_binary_tree.java
@@ -0,0 +1,34 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 606、根据二叉树创建字符串
+ *
+ * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+ * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+ */
+public class T0606_construct_string_from_binary_tree {
+
+    public String tree2Str(TreeNode root) {
+        return null;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
