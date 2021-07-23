cc8712e913e5be4eed4d1bc30ab7b982fdca1641
RickJinny <rickjinny@126.com>
2021-07-23 23:14:05 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0620_not_boring_movies.java
+++ src/com/rickjinny/leetcode/T0620_not_boring_movies.java
@@ -31,7 +31,8 @@ package com.rickjinny.leetcode;
 public class T0620_not_boring_movies {
 
     public static void main(String[] args) {
-        
+        String SQL = "select * from cinema where mod(id, 2) = 1 and description != 'boring' order by rating desc";
+        System.out.println(SQL);
     }
 
 }
