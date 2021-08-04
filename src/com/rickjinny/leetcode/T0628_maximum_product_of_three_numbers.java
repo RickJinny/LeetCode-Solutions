bfa1abecce855d64e87e906fa6ac387188ac581a
RickJinny <rickjinny@126.com>
2021-08-05 00:04:25 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0628_maximum_product_of_three_numbers.java
+++ src/com/rickjinny/leetcode/T0628_maximum_product_of_three_numbers.java
@@ -1,5 +1,28 @@
 package com.rickjinny.leetcode;
 
+/**
+ * 628、三个数的最大乘积
+ * 给你一个整型数组 nums, 在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ * 示例1:
+ * 输入: nums = [1, 2, 3]
+ * 输出: 6
+ *
+ * 示例2:
+ * 输入: nums = [1, 2, 3, 4]
+ * 输出: 24
+ *
+ * 示例3:
+ * 输入: nums = [-1, -2, -3]
+ * 输出: -6
+ *
+ * 提示:
+ *   3 <= nums.length <= 10^4
+ *   -1000 <= num[i] <= 1000
+ *
+ */
 public class T0628_maximum_product_of_three_numbers {
-    
+
+    public int maximumProduct(int[] nums) {
+        return 0;
+    }
 }
