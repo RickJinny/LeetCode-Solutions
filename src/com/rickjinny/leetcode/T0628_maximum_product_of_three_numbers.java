515f9df4c552711c0fb6f224830848fdd3a63add
RickJinny <rickjinny@126.com>
2021-08-07 09:30:11 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0628_maximum_product_of_three_numbers.java
+++ src/com/rickjinny/leetcode/T0628_maximum_product_of_three_numbers.java
@@ -22,7 +22,39 @@ package com.rickjinny.leetcode;
  */
 public class T0628_maximum_product_of_three_numbers {
 
-    public int maximumProduct(int[] nums) {
-        return 0;
+    public static int maximumProduct(int[] nums) {
+        // 最小的 和 第二小的
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        // 最大的、第二大的 和 第三大的
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+        }
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, -2, -3};
+        System.out.println(maximumProduct(nums));
     }
 }
