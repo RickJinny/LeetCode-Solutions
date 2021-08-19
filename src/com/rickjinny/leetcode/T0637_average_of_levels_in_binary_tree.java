2ba6416eceb59d0f55e8ccef5ee1b4264549232c
RickJinny <rickjinny@126.com>
2021-08-19 23:46:09 +0800

    LeetCode0637
    LeetCode0637

--- src/com/rickjinny/leetcode/T0637_average_of_levels_in_binary_tree.java
+++ src/com/rickjinny/leetcode/T0637_average_of_levels_in_binary_tree.java
@@ -1,5 +1,6 @@
 package com.rickjinny.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,8 +23,33 @@ import java.util.List;
  */
 public class T0637_average_of_levels_in_binary_tree {
 
-    public List<Double> averageOfLevels() {
-        return null;
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Integer> counts = new ArrayList<>();
+        List<Double> sums = new ArrayList<>();
+        dfs(root, 0, counts, sums);
+        List<Double> averages = new ArrayList<>();
+        int size = sums.size();
+        for (int i = 0; i < size; i++) {
+            averages.add(sums.get(i) / counts.get(i));
+        }
+        return averages;
+    }
+
+    public void dfs(TreeNode root, int level, List<Integer> counts, List<Double> sums) {
+        if (root == null) {
+            return;
+        }
+
+        if (level < sums.size()) {
+            sums.set(level, sums.get(level) + root.val);
+            counts.set(level, counts.get(level) + 1);
+        } else {
+            sums.add(1.0 * root.val);
+            counts.add(1);
+        }
+
+        dfs(root.left, level + 1, counts, sums);
+        dfs(root.right, level + 1, counts, sums);
     }
 
     public static class TreeNode {
