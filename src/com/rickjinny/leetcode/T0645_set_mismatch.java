c2954e2a6d05d448ac3f8e469f57baec6fdc0287
RickJinny <rickjinny@126.com>
2021-09-01 00:16:26 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0645_set_mismatch.java
+++ src/com/rickjinny/leetcode/T0645_set_mismatch.java
@@ -1,11 +1,52 @@
 package com.rickjinny.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 645、错误的集合
+ * 集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，
+ * 导致集合 丢失了一个数字 并且 有一个数字重复。
+ * 给定一个数组 nums 代表了集合 s 发生错误后的结果。
+ * 请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+ *
+ * 示例1:
+ * 输入: nums = [1, 2, 2, 4]
+ * 输出: [2, 3]
+ *
+ * 示例2:
+ * 输入: nums = [1, 1]
+ * 输出: [1, 2]
+ *
  */
 public class T0645_set_mismatch {
 
-    public int[] findErrorNums(int[] nums) {
-        return null;
+    public static int[] findErrorNums(int[] nums) {
+        Map<String, Integer> map = new HashMap<>(16);
+        for (int num : nums) {
+            String key = String.valueOf(num);
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        int[] result = new int[2];
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 2) {
+                int num = Integer.parseInt(entry.getKey());
+                result[0] = num;
+                result[1] = num + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 4};
+        int[] errorNums = findErrorNums(nums);
+        System.out.println(Arrays.toString(errorNums));
     }
 }
