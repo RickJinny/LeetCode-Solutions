ddae53f47a4fa7d359cfefc1a0258cd720465346
RickJinny <rickjinny@126.com>
2021-09-04 16:44:37 +0800

    LeetCode657
    LeetCode657

---
+++ src/com/rickjinny/leetcode/T0657_robot_return_to_origin.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 657、机器人能否返回原点
+ */
+public class T0657_robot_return_to_origin {
+
+    public boolean judgeCircle(String moves) {
+        return false;
+    }
+}
