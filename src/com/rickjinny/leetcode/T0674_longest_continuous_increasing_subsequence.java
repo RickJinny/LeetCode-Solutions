55599e5b79eb7c95a834c66a25ad690ce3ac3782
RickJinny <rickjinny@126.com>
2021-09-15 23:32:02 +0800

    LeetCode0674
    LeetCode0674

--- src/com/rickjinny/leetcode/T0674_longest_continuous_increasing_subsequence.java
+++ src/com/rickjinny/leetcode/T0674_longest_continuous_increasing_subsequence.java
@@ -6,7 +6,15 @@ package com.rickjinny.leetcode;
 public class T0674_longest_continuous_increasing_subsequence {
 
     public int findLengthOfLCIS(int[] nums) {
-        
-        return 0;
+        int ans = 0;
+        int n = nums.length;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] <= nums[i - 1]) {
+                start = i;
+            }
+            ans = Math.max(ans, i - start + 1);
+        }
+        return ans;
     }
 }
