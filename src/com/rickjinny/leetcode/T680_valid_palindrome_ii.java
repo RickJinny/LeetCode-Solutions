db4748faaf9d74e90c085f3a5f9de626712dafb8
RickJinny <rickjinny@126.com>
2021-10-03 08:40:15 +0800

    LeetCode680
    LeetCode680

---
+++ src/com/rickjinny/leetcode/T680_valid_palindrome_ii.java
@@ -0,0 +1,30 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 680、验证回文字符串 II
+ * 给定一个非空字符串 s, 最多删除一个字符。判断是否能成为回文字符串。
+ *
+ * 示例1:
+ * 输入: s = "aba"
+ * 输出: true
+ *
+ * 示例2:
+ * 输入: s = "abca"
+ * 输出: true
+ * 解释: 你可以删除 c 字符
+ *
+ * 示例3:
+ * 输入: s = "abc"
+ * 输出: false
+ *
+ * 提示:
+ * ① 1 <= s.length <=  10^5
+ * ② s 由小写英文字母组成
+ *
+ */
+public class T680_valid_palindrome_ii {
+
+    public boolean validPalindrome(String s) {
+        return false;
+    }
+}
