8f75df1f4cf88bb9fe09b5193bb013aecf552f77
RickJinny <rickjinny@126.com>
2021-10-06 16:44:27 +0800

    LeetCode680
    LeetCode680

--- src/com/rickjinny/leetcode/T680_valid_palindrome_ii.java
+++ src/com/rickjinny/leetcode/T680_valid_palindrome_ii.java
@@ -24,7 +24,26 @@ package com.rickjinny.leetcode;
  */
 public class T680_valid_palindrome_ii {
 
+    //记录删除的字符次数
+    int del = 0;
+
     public boolean validPalindrome(String s) {
-        return false;
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                // 不相等的话，若没有删除字符，则删除左边或右边的字符再判断；若删除过一次，则不是回文串
+                if (del == 0) {
+                    del++;
+                    return validPalindrome(s.substring(i, j)) || validPalindrome(s.substring(i + 1, j + 1));
+                }
+                return false;
+            }
+        }
+
+        return true;
     }
 }
