c231269050bb5a64f50f8ab36e18e9265bb4c4e9
RickJinny <rickjinny@126.com>
2021-11-09 22:11:06 +0800

    LeetCode0693
    LeetCode0693

---
+++ src/com/rickjinny/leetcode/T0693_binary_number_with_alternating_bits.java
@@ -0,0 +1,40 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 693、交替位二进制数
+ * 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字
+ * 永不相同。
+ *
+ * 示例1:
+ * 输入: n = 5
+ * 输出: true
+ * 解释: 5 的二进制表示是: 10
+ *
+ * 示例2:
+ * 输入: n = 7
+ * 输出: false
+ * 解释: 7 的二进制表示是: 111
+ *
+ * 示例3:
+ * 输入: n = 11
+ * 输出: false
+ * 解释: 11 的二进制表示是: 1011
+ *
+ * 示例4:
+ * 输入: n = 10
+ * 输出: true
+ * 解释: 10 的二进制表示是: 1010
+ *
+ * 示例5:
+ * 输入: n = 3
+ * 输出: false
+ *
+ * 提示: 1 <= n <= 2^31 - 1
+ *
+ */
+public class T0693_binary_number_with_alternating_bits {
+
+    public boolean hasAlternatingBits(int n) {
+        return false;
+    }
+}
