d45c17edbdec53e6f300aa7afefbc6b9f079be85
RickJinny <rickjinny@126.com>
2021-12-17 23:22:50 +0800

    update LeetCode0700
    update LeetCode0700

--- src/com/rickjinny/leetcode/T0700_search_in_a_binary_search_tree.java
+++ src/com/rickjinny/leetcode/T0700_search_in_a_binary_search_tree.java
@@ -25,7 +25,14 @@ package com.rickjinny.leetcode;
 public class T0700_search_in_a_binary_search_tree {
 
     public TreeNode searchBST(TreeNode root, int val) {
-        return null;
+        if (root == null) {
+            return null;
+        }
+
+        if (val == root.val) {
+            return root;
+        }
+        return searchBST(val < root.val ? root.left : root.right, val);
     }
 
     public static class TreeNode {
