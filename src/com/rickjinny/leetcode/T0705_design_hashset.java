f80d552f9c985fa017f0ff55017cde82ff5ac048
RickJinny <rickjinny@126.com>
2022-01-05 00:12:20 +0800

    LeetCode0705
    LeetCode0705

---
+++ src/com/rickjinny/leetcode/T0705_design_hashset.java
@@ -0,0 +1,24 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 705、设计哈希集合
+ */
+public class T0705_design_hashset {
+
+    public T0705_design_hashset() {
+
+    }
+
+    public void add(int key) {
+
+    }
+
+    public void remove(int key) {
+
+    }
+
+    public boolean contains(int keys) {
+        return false;
+    }
+
+}
