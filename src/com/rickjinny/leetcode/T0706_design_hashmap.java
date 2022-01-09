a9230badb79cda7257fcc5f9c8eed8bb44b1283c
RickJinny <rickjinny@126.com>
2022-01-10 00:27:13 +0800

    LeetCode0706
    LeetCode0706

---
+++ src/com/rickjinny/leetcode/T0706_design_hashmap.java
@@ -0,0 +1,17 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 706、设计哈希映射
+ * 不使用 任何内建的 哈希表库，设计一个哈希映射（HashMap）。
+ *
+ * 实现 MyHashMap 类:
+ * (1)、MyHashMap() 用空映射初始化对象。
+ * (2)、void put(int key, int value) 向 HashMap 插入一个键值对 (key, value)。
+ *      如果 key 已经存在于映射中，则更新其对应的值 value。
+ * (3)、int get(int key) 返回特定的 key 所映射的 value; 如果映射中不包含 key 的映射，返回 -1。
+ * (4)、void remove(key) 如果映射中存在 key 的映射，则移除 key 和它所对应的 value。
+ * 
+ */
+public class T0706_design_hashmap {
+
+}
