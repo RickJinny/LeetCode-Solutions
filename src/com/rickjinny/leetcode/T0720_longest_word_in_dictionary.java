1c7b73aecc1dc26b456628fceebff02be34b2285
RickJinny <rickjinny@126.com>
2022-01-19 23:20:12 +0800

    LeetCode0720
    LeetCode0720

---
+++ src/com/rickjinny/leetcode/T0720_longest_word_in_dictionary.java
@@ -0,0 +1,12 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 720、词典中最长的单词
+ */
+public class T0720_longest_word_in_dictionary {
+
+    public String longestWord(String[] words) {
+        return "";
+    }
+
+}
