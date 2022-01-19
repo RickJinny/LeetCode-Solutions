b4b484cbea3427c9189001f5f4501c9c58c5d60d
RickJinny <rickjinny@126.com>
2022-01-20 00:06:25 +0800

    update
    update

--- src/com/rickjinny/leetcode/T0720_longest_word_in_dictionary.java
+++ src/com/rickjinny/leetcode/T0720_longest_word_in_dictionary.java
@@ -2,6 +2,20 @@ package com.rickjinny.leetcode;
 
 /**
  * 720、词典中最长的单词
+ * 给出一个字符串数组 words 组成的一本英语词典。从中找出最长的一个单词，该单词是由 words 词典中其他单词
+ * 逐步添加一个字母组成。若其中有多个可行的答案，则返回答案中字母序最小的单词。
+ * 若无答案，则返回空字符串。
+ *
+ * 示例1:
+ * 输入: words = ["w", "wo", "wor", "world"]
+ * 输出: "words"
+ * 解释: 单词 "world" 可由 "w"、"wo"、"wor" 和 "worl" 添加一个字母组成。
+ *
+ * 示例2:
+ * 输入: words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+ * 输出: "apple"
+ * 解释: "apply" 和 "apple" 都能由词典中的单词组成。但是 "apple" 的字典序小于 "apply"。
+ *
  */
 public class T0720_longest_word_in_dictionary {
 
