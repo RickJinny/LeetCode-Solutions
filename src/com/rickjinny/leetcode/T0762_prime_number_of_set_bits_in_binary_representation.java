c23ca8c8c922f0f25257798ae899f3c567434364
RickJinny <rickjinny@126.com>
2022-02-10 00:22:26 +0800

    LeetCode0762
    LeetCode0762

---
+++ src/com/rickjinny/leetcode/T0762_prime_number_of_set_bits_in_binary_representation.java
@@ -0,0 +1,12 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 762、二进制表示中质数个计算置位
+ */
+public class T0762_prime_number_of_set_bits_in_binary_representation {
+
+    public int countPrimeSetBits(int left, int right) {
+        return 0;
+    }
+
+}
