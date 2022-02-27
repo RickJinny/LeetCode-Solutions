6bde3e2553ae22980c639121cd1e166104e743a9
RickJinny <rickjinny@126.com>
2022-02-27 22:39:10 +0800

    LeetCode0766
    LeetCode0766

---
+++ src/com/rickjinny/leetcode/T0766_toeplitz_matrix.java
@@ -0,0 +1,11 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 766、托普利茨矩阵
+ */
+public class T0766_toeplitz_matrix {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        return false;
+    }
+}
