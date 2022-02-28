767eb874348a7066ac488b30802d13888c76c5df
RickJinny <rickjinny@126.com>
2022-02-28 23:16:12 +0800

    Update0766
    Update0766

--- src/com/rickjinny/leetcode/T0766_toeplitz_matrix.java
+++ src/com/rickjinny/leetcode/T0766_toeplitz_matrix.java
@@ -6,6 +6,16 @@ package com.rickjinny.leetcode;
 public class T0766_toeplitz_matrix {
 
     public boolean isToeplitzMatrix(int[][] matrix) {
-        return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+    
 }
