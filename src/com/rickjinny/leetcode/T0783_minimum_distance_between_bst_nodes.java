c1f0ae53016cdc10133c32badf77c8d11be7e2a9
RickJinny <rickjinny@126.com>
2022-03-07 00:33:21 +0800

    LeetCode0783
    LeetCode0783

---
+++ src/com/rickjinny/leetcode/T0783_minimum_distance_between_bst_nodes.java
@@ -0,0 +1,33 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 783、二叉搜索树节点最小距离
+ */
+public class T0783_minimum_distance_between_bst_nodes {
+
+    public int minDiffInBST(TreeNode root) {
+
+        
+        return 0;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
