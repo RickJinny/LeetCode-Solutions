754f2f5b34267ae4d49fe50b0fe9bde642d1c2fb
RickJinny <rickjinny@126.com>
2022-03-06 16:54:19 +0800

    update0771
    update0771

--- src/com/rickjinny/leetcode/T0771_jewels_and_stones.java
+++ src/com/rickjinny/leetcode/T0771_jewels_and_stones.java
@@ -22,7 +22,20 @@ package com.rickjinny.leetcode;
 public class T0771_jewels_and_stones {
 
     public int numJewelsInStones(String jewels, String stones) {
-        return 0;
+        int jewelsCount = 0;
+        int jewelsLength = jewels.length();
+        int stonesLength = stones.length();
+        for (int i = 0; i < stonesLength; i++) {
+            char stone = stones.charAt(i);
+            for (int j = 0; j < jewelsLength; j++) {
+                char jewel = jewels.charAt(j);
+                if (stone == jewel) {
+                    jewelsCount++;
+                    break;
+                }
+            }
+        }
+        return jewelsCount;
     }
-    
+
 }
