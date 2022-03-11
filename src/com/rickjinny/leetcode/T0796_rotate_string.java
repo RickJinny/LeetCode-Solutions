13f592114115079c5a7d1036e818381b6c3e2626
RickJinny <rickjinny@126.com>
2022-03-12 00:01:26 +0800

    Update0796
    Update0796

--- src/com/rickjinny/leetcode/T0796_rotate_string.java
+++ src/com/rickjinny/leetcode/T0796_rotate_string.java
@@ -2,7 +2,12 @@ package com.rickjinny.leetcode;
 
 /**
  * 796、旋转字符串
+ * 给定两个字符串，s 和 goal。如果在若干次旋转操作之后，s 能变成 goal，那么返回 true。
+ *
  */
 public class T0796_rotate_string {
 
+    public boolean roateString(String s, String goal) {
+        return false;
+    }
 }
