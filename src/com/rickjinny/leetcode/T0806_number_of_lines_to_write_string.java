44611e13f76c3cbdcf85a890f6081750332e3dd4
RickJinny <rickjinny@126.com>
2022-04-08 22:32:36 +0800

    LeetCode0806
    LeetCode0806

---
+++ src/com/rickjinny/leetcode/T0806_number_of_lines_to_write_string.java
@@ -0,0 +1,12 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 806、写字符串需要的行数
+ */
+public class T0806_number_of_lines_to_write_string {
+
+    public int[] numberOfLines(int[] widths, String s) {
+        
+        return null;
+    }
+}
