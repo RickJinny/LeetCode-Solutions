fd091b4844dd99009d2d125eb2ba14af57da194d
RickJinny <rickjinny@126.com>
2022-05-03 22:10:46 +0800

    update0806
    update0806

--- src/com/rickjinny/leetcode/T0806_number_of_lines_to_write_string.java
+++ src/com/rickjinny/leetcode/T0806_number_of_lines_to_write_string.java
@@ -5,8 +5,19 @@ package com.rickjinny.leetcode;
  */
 public class T0806_number_of_lines_to_write_string {
 
+    public static final int MAX_WIDTH = 100;
+
     public int[] numberOfLines(int[] widths, String s) {
-        
-        return null;
+        int lines = 1;
+        int width = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int need = widths[s.charAt(i) - 'a'];
+            width += need;
+            if (width > MAX_WIDTH) {
+                lines++;
+                width = need;
+            }
+        }
+        return new int[]{lines, width};
     }
 }
