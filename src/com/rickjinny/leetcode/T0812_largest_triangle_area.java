6608c4e44fedfab7466d5e9d2e4457268cd43193
RickJinny <rickjinny@126.com>
2022-06-04 15:55:32 +0800

    LeetCode0812
    LeetCode0812

---
+++ src/com/rickjinny/leetcode/T0812_largest_triangle_area.java
@@ -0,0 +1,20 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 812、最大三角形面积
+ * 给定包含多个点的集合，从其中取三个点组成三角形，返回能组成的最大三角形的面积。
+ *
+ * 示例:
+ * 输入: point = [[0, 0], [0, 1], [1, 0], [0, 2], [2, 0]]
+ * 输出: 2
+ * 解释: 这五个点如下图所示，组成的橙色三角形是最大的，面积为2。
+ *
+ *
+ */
+public class T0812_largest_triangle_area {
+
+    public double largestTriangleArea(int[][] points) {
+        return 0.0D;
+    }
+
+}
