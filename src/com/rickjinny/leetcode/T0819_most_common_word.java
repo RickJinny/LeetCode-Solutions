a15f2bf476046f456db1dea8988ea20c861b5ad3
RickJinny <rickjinny@126.com>
2022-06-18 17:09:41 +0800

    leetcode0819
    leetcode0819

---
+++ src/com/rickjinny/leetcode/T0819_most_common_word.java
@@ -0,0 +1,19 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 819、最常见的单词
+ * 给定一个段落 和 一个禁用单词列表。返回出现次数最多，同时不在禁用列表中的单词。
+ * 题目保证至少有一个不在禁用列表中，而且答案唯一。
+ * 禁用列表中的单词用小写字母表示，不含标点符号。段落中的单词不区分大小写。答案都是小写字母。
+ *
+ * 输入: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+ * banned = ["hit"]
+ * 输出: "ball"
+ */
+public class T0819_most_common_word {
+
+    public String mostCommonWord(String paragraph, String[] banned) {
+        return null;
+    }
+
+}
