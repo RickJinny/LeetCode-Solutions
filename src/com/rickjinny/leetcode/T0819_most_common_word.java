56e4c8c07a14bc121a930aed1231199e72ec886b
RickJinny <rickjinny@126.com>
2022-06-26 11:17:08 +0800

    Update0819
    Update0819

--- src/com/rickjinny/leetcode/T0819_most_common_word.java
+++ src/com/rickjinny/leetcode/T0819_most_common_word.java
@@ -1,5 +1,10 @@
 package com.rickjinny.leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 819、最常见的单词
  * 给定一个段落 和 一个禁用单词列表。返回出现次数最多，同时不在禁用列表中的单词。
@@ -13,7 +18,39 @@ package com.rickjinny.leetcode;
 public class T0819_most_common_word {
 
     public String mostCommonWord(String paragraph, String[] banned) {
-        return null;
+        Set<String> bannedSet = new HashSet<>();
+        for (String word : banned) {
+            bannedSet.add(word);
+        }
+        int maxFrequency = 0;
+        Map<String, Integer> frequencies = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        int length = paragraph.length();
+        for (int i = 0; i < length; i++) {
+            if (i < length && Character.isLetter(paragraph.charAt(i))) {
+                sb.append(Character.toLowerCase(paragraph.charAt(i)));
+            } else if (sb.length() > 0) {
+                String word = sb.toString();
+                if (!bannedSet.contains(word)) {
+                    int frequency = frequencies.getOrDefault(word, 0) + 1;
+                    frequencies.put(word, frequency);
+                    maxFrequency = Math.max(maxFrequency, frequency);
+                }
+                sb.setLength(0);
+            }
+        }
+
+        String mostCommon = "";
+        Set<Map.Entry<String, Integer>> entries = frequencies.entrySet();
+        for (Map.Entry<String, Integer> entry : entries) {
+            String word = entry.getKey();
+            Integer frequency = entry.getValue();
+            if (frequency == maxFrequency) {
+                mostCommon = word;
+                break;
+            }
+        }
+        return mostCommon;
     }
 
 }
