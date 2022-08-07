53eab6058334c1404c023bf606007628a5823b2d
RickJinny <rickjinny@126.com>
2022-08-07 23:34:36 +0800

    update0821
    update0821

--- src/com/rickjinny/leetcode/T0821_shortest_distance_to_a_character.java
+++ src/com/rickjinny/leetcode/T0821_shortest_distance_to_a_character.java
@@ -27,7 +27,23 @@ package com.rickjinny.leetcode;
 public class T0821_shortest_distance_to_a_character {
 
     public int[] shortestToChar(String s, char c) {
-        
-        return null;
+        int n = s.length();
+        int[] ans = new int[n];
+
+        for (int i = 0, idx = -n; i < n; ++i) {
+            if (s.charAt(i) == c) {
+                idx = i;
+            }
+            ans[i] = i - idx;
+        }
+
+        for (int i = n - 1, idx = 2 * n; i >= 0; --i) {
+            if (s.charAt(i) == c) {
+                idx = i;
+            }
+            ans[i] = Math.min(ans[i], idx - i);
+        }
+
+        return ans;
     }
 }
