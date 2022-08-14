67e94b7a381c92e214d9b78fad34406e9e66c27a
RickJinny <rickjinny@126.com>
2022-08-14 11:10:44 +0800

    LeetCode0824
    LeetCode0824

---
+++ src/com/rickjinny/leetcode/T0824_goat_latin.java
@@ -0,0 +1,26 @@
+package com.rickjinny.leetcode;
+
+/**
+ * 824、山羊拉丁文
+ *
+ * 示例1:
+ * 输入: sentence = "I speak Goat Latin"
+ * 输出: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+ *
+ * 示例2:
+ * 输入: sentence = "The quick brown fox jumped over the lazy dog"
+ * 输出: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ *
+ * 提示:
+ * （1）1 <= sentence.length <= 150
+ * （2）sentence 由英文字母和空格组成
+ * （3）sentence 不含前导或尾随空格
+ * （4）sentence 中的所有单词由单个空格分隔
+ *
+ */
+public class T0824_goat_latin {
+
+    public String toGoatLatin(String sentence) {
+        return "";
+    }
+}
