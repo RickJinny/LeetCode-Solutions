f426290eb6774c349f7dda04a99ac6f28282d280
RickJinny <rickjinny@126.com>
2022-08-19 23:56:10 +0800

    LeetCode0830
    LeetCode0830

---
+++ src/com/rickjinny/leetcode/T0830_positions_of_large_groups.java
@@ -0,0 +1,21 @@
+package com.rickjinny.leetcode;
+
+import java.util.List;
+
+/**
+ * 830、较大分组的位置
+ * 在一个由小写字母构成的字符串 s 中，包含由一些连续的相同字符所构成的分组。
+ * 例如: 在字符串 s = "abbxxxxzyy" 中，就含有 "a", "bb", "xxxx", "z" 和 "yy" 这样的一些分组。
+ *
+ */
+public class T0830_positions_of_large_groups {
+
+    public List<List<Integer>> largeGroupPositions(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
