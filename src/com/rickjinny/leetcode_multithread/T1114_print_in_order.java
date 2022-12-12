7026b2332a7992536e4973e4ccdf6e28139669b8
RickJinny <rickjinny@126.com>
2022-12-12 23:12:58 +0800

    update1114
    update1114

--- src/com/rickjinny/leetcode_multithread/T1114_print_in_order.java
+++ src/com/rickjinny/leetcode_multithread/T1114_print_in_order.java
@@ -4,40 +4,40 @@ import java.util.concurrent.CountDownLatch;
 
 /**
  * 1114、按序打印
- * 给你一个类：
+ * 方法一: CountDownLatch (减法计数器)
  */
 public class T1114_print_in_order {
 
     static class Foo {
-
-        CountDownLatch latch12 = new CountDownLatch(1);
-        CountDownLatch latch23 = new CountDownLatch(1);
+        CountDownLatch countDownLatch12;
+        CountDownLatch countDownLatch23;
 
         public Foo() {
-
+            countDownLatch12 = new CountDownLatch(1);
+            countDownLatch23 = new CountDownLatch(1);
         }
 
         public void first(Runnable printFirst) throws InterruptedException {
             // printFirst.run() outputs "first". Do not change or remove this line.
             printFirst.run();
             // 唤醒线程2
-            latch12.countDown();
+            countDownLatch12.countDown();
         }
 
         public void second(Runnable printSecond) throws InterruptedException {
             // printFirst.run() outputs "second". Do not change or remove this line.
 
             // latch12 的值为 0 会执行下面的语句，否则会在此阻塞
-            latch12.await();
+            countDownLatch12.await();
             printSecond.run();
             // 唤醒线程3
-            latch23.countDown();
+            countDownLatch23.countDown();
         }
 
         public void third(Runnable printThird) throws InterruptedException {
             // printFirst.run() outputs "third". Do not change or remove this line.
             // latch23 的值为 0 会执行下面的语句，否则会在此阻塞
-            latch23.await();
+            countDownLatch23.await();
             printThird.run();
         }
     }
